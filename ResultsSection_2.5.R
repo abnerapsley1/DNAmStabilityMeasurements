b278622787eeaa42e8ca70ae777bdb2380034c23
abnerapsley1 <57500819+abnerapsley1@users.noreply.github.com>
2023-06-07 14:43:30 -0400

    Update ResultsSection_2.5.R

--- ResultsSection_2.5.R
+++ ResultsSection_2.5.R
@@ -15,215 +15,65 @@ library(naniar)
 ############################################################################
 
 ### Read in Data ### ----
-load("scen5a_icc_s0_t1t2t3t4_ela0.RData")
-scen5a.ela0.icc.comb <- dplyr::filter(scen5a.ela0.icc.comb, scen5a.ela0.icc.comb$covariates == "Adjusted for DNAm_Monocytes")
-scen5a.ela0.icc.comb <- dplyr::filter(scen5a.ela0.icc.comb, scen5a.ela0.icc.comb$type == "ICC(2,1)")[,c(1,5)]
-colnames(scen5a.ela0.icc.comb)[2] <- "ICC_Scen5_ELA0"
-load("scen5b_icc_s0_t1t2t3t4_ela1.RData")
-scen5b.ela1.icc.comb <- dplyr::filter(scen5b.ela1.icc.comb, scen5b.ela1.icc.comb$covariates == "Adjusted for DNAm_Monocytes")
-scen5b.ela1.icc.comb <- dplyr::filter(scen5b.ela1.icc.comb, scen5b.ela1.icc.comb$type == "ICC(2,1)")[,c(1,5)]
-colnames(scen5b.ela1.icc.comb)[2] <- "ICC_Scen5_ELA1"
+### Read in Data ### ----
+load("scen6a_icc_s1_t1t2_ela0_covar2_revision.RData")
+icc.comb.scen6a.covar2 <- dplyr::filter(icc.comb.scen6a.covar2, icc.comb.scen6a.covar2$covariates == "Adjusted for DNAm_Monocytes, Methylation_Batch")
+icc.comb.scen6a.covar2 <- dplyr::filter(icc.comb.scen6a.covar2, icc.comb.scen6a.covar2$type == "ICC(2,1)")[,c(1,5)]
+colnames(icc.comb.scen6a.covar2)[2] <- "ICC_Scen6_ELA0"
+load("scen6b_icc_s1_t1t2_ela1_covar2_revision.RData")
+icc.comb.scen6b.covar2 <- dplyr::filter(icc.comb.scen6b.covar2, icc.comb.scen6b.covar2$covariates == "Adjusted for DNAm_Monocytes, Methylation_Batch")
+icc.comb.scen6b.covar2 <- dplyr::filter(icc.comb.scen6b.covar2, icc.comb.scen6b.covar2$type == "ICC(2,1)")[,c(1,5)]
+colnames(icc.comb.scen6b.covar2)[2] <- "ICC_Scen6_ELA1"
 
-load("scen6a_icc_s1_t1t2_ela0.RData")
-scen6a.ela0.icc.comb <- dplyr::filter(scen6a.ela0.icc.comb, scen6a.ela0.icc.comb$covariates == "Adjusted for DNAm_Monocytes")
-scen6a.ela0.icc.comb <- dplyr::filter(scen6a.ela0.icc.comb, scen6a.ela0.icc.comb$type == "ICC(2,1)")[,c(1,5)]
-colnames(scen6a.ela0.icc.comb)[2] <- "ICC_Scen6_ELA0"
-load("scen6b_icc_s1_t1t2_ela1.RData")
-scen6b.ela1.icc.comb <- dplyr::filter(scen6b.ela1.icc.comb, scen6b.ela1.icc.comb$covariates == "Adjusted for DNAm_Monocytes")
-scen6b.ela1.icc.comb <- dplyr::filter(scen6b.ela1.icc.comb, scen6b.ela1.icc.comb$type == "ICC(2,1)")[,c(1,5)]
-colnames(scen6b.ela1.icc.comb)[2] <- "ICC_Scen6_ELA1"
+load("scen7a_icc_s1_t1t3_ela0_covar2_revision.RData")
+icc.comb.scen7a.covar2 <- dplyr::filter(icc.comb.scen7a.covar2, icc.comb.scen7a.covar2$covariates == "Adjusted for DNAm_Monocytes, Methylation_Batch")
+icc.comb.scen7a.covar2 <- dplyr::filter(icc.comb.scen7a.covar2, icc.comb.scen7a.covar2$type == "ICC(2,1)")[,c(1,5)]
+colnames(icc.comb.scen7a.covar2)[2] <- "ICC_Scen7_ELA0"
+load("scen7b_icc_s1_t1t3_ela1_covar2_revision.RData")
+icc.comb.scen7b.covar2 <- dplyr::filter(icc.comb.scen7b.covar2, icc.comb.scen7b.covar2$covariates == "Adjusted for DNAm_Monocytes, Methylation_Batch")
+icc.comb.scen7b.covar2 <- dplyr::filter(icc.comb.scen7b.covar2, icc.comb.scen7b.covar2$type == "ICC(2,1)")[,c(1,5)]
+colnames(icc.comb.scen7b.covar2)[2] <- "ICC_Scen7_ELA1"
 
-load("scen7a_icc_s1_t1t3_ela0.RData")
-scen7a.ela0.icc.comb <- dplyr::filter(scen7a.ela0.icc.comb, scen7a.ela0.icc.comb$covariates == "Adjusted for DNAm_Monocytes")
-scen7a.ela0.icc.comb <- dplyr::filter(scen7a.ela0.icc.comb, scen7a.ela0.icc.comb$type == "ICC(2,1)")[,c(1,5)]
-colnames(scen7a.ela0.icc.comb)[2] <- "ICC_Scen7_ELA0"
-load("scen7b_icc_s1_t1t3_ela1.RData")
-scen7b.ela1.icc.comb <- dplyr::filter(scen7b.ela1.icc.comb, scen7b.ela1.icc.comb$covariates == "Adjusted for DNAm_Monocytes")
-scen7b.ela1.icc.comb <- dplyr::filter(scen7b.ela1.icc.comb, scen7b.ela1.icc.comb$type == "ICC(2,1)")[,c(1,5)]
-colnames(scen7b.ela1.icc.comb)[2] <- "ICC_Scen7_ELA1"
+load("scen8a_icc_s1_t1t4_ela0_covar2_revision.RData")
+icc.comb.scen8a.covar2 <- dplyr::filter(icc.comb.scen8a.covar2, icc.comb.scen8a.covar2$covariates == "Adjusted for DNAm_Monocytes, Methylation_Batch")
+icc.comb.scen8a.covar2 <- dplyr::filter(icc.comb.scen8a.covar2, icc.comb.scen8a.covar2$type == "ICC(2,1)")[,c(1,5)]
+colnames(icc.comb.scen8a.covar2)[2] <- "ICC_Scen8_ELA0"
+load("scen8b_icc_s1_t1t4_ela1_covar2_revision.RData")
+icc.comb.scen8b.covar2 <- dplyr::filter(icc.comb.scen8b.covar2, icc.comb.scen8b.covar2$covariates == "Adjusted for DNAm_Monocytes, Methylation_Batch")
+icc.comb.scen8b.covar2 <- dplyr::filter(icc.comb.scen8b.covar2, icc.comb.scen8b.covar2$type == "ICC(2,1)")[,c(1,5)]
+colnames(icc.comb.scen8b.covar2)[2] <- "ICC_Scen8_ELA1"
 
-load("scen8a_icc_s1_t1t4_ela0.RData")
-scen8a.ela0.icc.comb <- dplyr::filter(scen8a.ela0.icc.comb, scen8a.ela0.icc.comb$covariates == "Adjusted for DNAm_Monocytes")
-scen8a.ela0.icc.comb <- dplyr::filter(scen8a.ela0.icc.comb, scen8a.ela0.icc.comb$type == "ICC(2,1)")[,c(1,5)]
-colnames(scen8a.ela0.icc.comb)[2] <- "ICC_Scen8_ELA0"
-load("scen8b_icc_s1_t1t4_ela1.RData")
-scen8b.ela1.icc.comb <- dplyr::filter(scen8b.ela1.icc.comb, scen8b.ela1.icc.comb$covariates == "Adjusted for DNAm_Monocytes")
-scen8b.ela1.icc.comb <- dplyr::filter(scen8b.ela1.icc.comb, scen8b.ela1.icc.comb$type == "ICC(2,1)")[,c(1,5)]
-colnames(scen8b.ela1.icc.comb)[2] <- "ICC_Scen8_ELA1"
-
-load("scen9a_icc_s1_t3t4_ela0.RData")
-scen9a.ela0.icc.comb <- dplyr::filter(scen9a.ela0.icc.comb, scen9a.ela0.icc.comb$covariates == "Adjusted for DNAm_Monocytes")
-scen9a.ela0.icc.comb <- dplyr::filter(scen9a.ela0.icc.comb, scen9a.ela0.icc.comb$type == "ICC(2,1)")[,c(1,5)]
-colnames(scen9a.ela0.icc.comb)[2] <- "ICC_Scen9_ELA0"
-load("scen9b_icc_s1_t3t4_ela1.RData")
-scen9b.ela1.icc.comb <- dplyr::filter(scen9b.ela1.icc.comb, scen9b.ela1.icc.comb$covariates == "Adjusted for DNAm_Monocytes")
-scen9b.ela1.icc.comb <- dplyr::filter(scen9b.ela1.icc.comb, scen9b.ela1.icc.comb$type == "ICC(2,1)")[,c(1,5)]
-colnames(scen9b.ela1.icc.comb)[2] <- "ICC_Scen9_ELA1"
+load("scen9a_icc_s1_t3t4_ela0_covar2_revision.RData")
+icc.comb.scen9a.covar2 <- dplyr::filter(icc.comb.scen9a.covar2, icc.comb.scen9a.covar2$covariates == "Adjusted for DNAm_Monocytes, Methylation_Batch")
+icc.comb.scen9a.covar2 <- dplyr::filter(icc.comb.scen9a.covar2, icc.comb.scen9a.covar2$type == "ICC(2,1)")[,c(1,5)]
+colnames(icc.comb.scen9a.covar2)[2] <- "ICC_Scen9_ELA0"
+load("scen9b_icc_s1_t3t4_ela1_covar2_revision.RData")
+icc.comb.scen9b.covar2 <- dplyr::filter(icc.comb.scen9b.covar2, icc.comb.scen9b.covar2$covariates == "Adjusted for DNAm_Monocytes, Methylation_Batch")
+icc.comb.scen9b.covar2 <- dplyr::filter(icc.comb.scen9b.covar2, icc.comb.scen9b.covar2$type == "ICC(2,1)")[,c(1,5)]
+colnames(icc.comb.scen9b.covar2)[2] <- "ICC_Scen9_ELA1"
 
 ### Merge all Data ### ----
-data <- merge(scen5a.ela0.icc.comb, scen5b.ela1.icc.comb, by = "DNAm.probe")
-data <- merge(data, scen6a.ela0.icc.comb, by = "DNAm.probe")
-data <- merge(data, scen6b.ela1.icc.comb, by = "DNAm.probe")
-data <- merge(data, scen7a.ela0.icc.comb, by = "DNAm.probe")
-data <- merge(data, scen7b.ela1.icc.comb, by = "DNAm.probe")
-data <- merge(data, scen8a.ela0.icc.comb, by = "DNAm.probe")
-data <- merge(data, scen8b.ela1.icc.comb, by = "DNAm.probe")
-data <- merge(data, scen9a.ela0.icc.comb, by = "DNAm.probe")
-data <- merge(data, scen9b.ela1.icc.comb, by = "DNAm.probe")
+data <- merge(icc.comb.scen6a.covar2, icc.comb.scen6b.covar2, by = "DNAm.probe")
+data <- merge(data, icc.comb.scen7a.covar2, by = "DNAm.probe")
+data <- merge(data, icc.comb.scen7b.covar2, by = "DNAm.probe")
+data <- merge(data, icc.comb.scen8a.covar2, by = "DNAm.probe")
+data <- merge(data, icc.comb.scen8b.covar2, by = "DNAm.probe")
+data <- merge(data, icc.comb.scen9a.covar2, by = "DNAm.probe")
+data <- merge(data, icc.comb.scen9b.covar2, by = "DNAm.probe")
 Exclude_Probes_Comb <- readRDS("Exclude_Probes_Comb.Rds")
 keep <- !(data$DNAm.probe %in% Exclude_Probes_Comb$DNAm.probe)
 data <- data[keep,]
 
 # Delete old dataframes #
-scen5a.ela0.icc.comb <- NULL
-scen5b.ela1.icc.comb <- NULL
-scen6a.ela0.icc.comb <- NULL
-scen6b.ela1.icc.comb <- NULL
-scen7a.ela0.icc.comb <- NULL
-scen7b.ela1.icc.comb <- NULL
-scen8a.ela0.icc.comb <- NULL
-scen8b.ela1.icc.comb <- NULL
-scen9a.ela0.icc.comb <- NULL
-scen9b.ela1.icc.comb <- NULL
+icc.comb.scen6a.covar2 <- NULL
+icc.comb.scen6b.covar2 <- NULL
+icc.comb.scen7a.covar2 <- NULL
+icc.comb.scen7b.covar2 <- NULL
+icc.comb.scen8a.covar2 <- NULL
+icc.comb.scen8b.covar2 <- NULL
+icc.comb.scen9a.covar2 <- NULL
+icc.comb.scen9b.covar2 <- NULL
 
-### Generate Summary Statistics ### ----
-describe(data[,2:11])
-
-### Generate Combined Histogram Plot ### ----
-hist_5a <- ggplot(data, aes(x=ICC_Scen5_ELA0)) + 
-  geom_histogram(color="black", fill="white") +
-  geom_vline(aes(xintercept=0.41),
-             color="black", linetype="dashed", size=1) +
-  theme_bw() +
-  ylim(0,100000) +
-  labs(title = "Scenario 5", x = NULL, y = NULL) +
-  theme(axis.text.x=element_blank(), #remove x axis labels
-        axis.ticks.x=element_blank(),
-        plot.title = element_text(hjust = 0.5), #remove x axis ticks
-        axis.text.y=element_blank(),  #remove y axis labels
-        axis.ticks.y=element_blank())  #remove y axis ticks
-
-hist_5b <- ggplot(data, aes(x=ICC_Scen5_ELA1)) + 
-  geom_histogram(color="black", fill="white") +
-  geom_vline(aes(xintercept=0.53),
-             color="black", linetype="dashed", size=1) +
-  theme_bw() +
-  ylim(0,100000) +
-  labs(title = NULL, x = NULL, y = NULL) +
-  theme(axis.text.x=element_blank(), #remove x axis labels
-        axis.ticks.x=element_blank(), #remove x axis ticks
-        axis.text.y=element_blank(),  #remove y axis labels
-        axis.ticks.y=element_blank(), #remove y axis ticks
-        plot.title = element_text(hjust = 0.5))  
-
-hist_6a <- ggplot(data, aes(x=ICC_Scen6_ELA0)) + 
-  geom_histogram(color="black", fill="white") +
-  geom_vline(aes(xintercept=0.53),
-             color="black", linetype="dashed", size=1) +
-  theme_bw() +
-  ylim(0,100000) +
-  labs(title = "Scenario 6", x = NULL, y = NULL) +
-  theme(axis.text.x=element_blank(), #remove x axis labels
-        axis.ticks.x=element_blank(),
-        plot.title = element_text(hjust = 0.5), #remove x axis ticks
-        axis.text.y=element_blank(),  #remove y axis labels
-        axis.ticks.y=element_blank())  #remove y axis ticks
-
-hist_6b <- ggplot(data, aes(x=ICC_Scen6_ELA1)) + 
-  geom_histogram(color="black", fill="white") +
-  geom_vline(aes(xintercept=0.39),
-             color="black", linetype="dashed", size=1) +
-  theme_bw() +
-  ylim(0,100000) +
-  labs(title = NULL, x = NULL, y = NULL) +
-  theme(axis.text.x=element_blank(), #remove x axis labels
-        axis.ticks.x=element_blank(), #remove x axis ticks
-        axis.text.y=element_blank(),  #remove y axis labels
-        axis.ticks.y=element_blank(), #remove y axis ticks
-        plot.title = element_text(hjust = 0.5))  
-
-hist_7a <- ggplot(data, aes(x=ICC_Scen7_ELA0)) + 
-  geom_histogram(color="black", fill="white") +
-  geom_vline(aes(xintercept=0.49),
-             color="black", linetype="dashed", size=1) +
-  theme_bw() +
-  ylim(0,100000) +
-  labs(title = "Scenario 7", x = NULL, y = NULL) +
-  theme(axis.text.x=element_blank(), #remove x axis labels
-        axis.ticks.x=element_blank(), #remove x axis ticks
-        axis.text.y=element_blank(),  #remove y axis labels
-        axis.ticks.y=element_blank(), #remove y axis ticks
-        plot.title = element_text(hjust = 0.5))  
-
-hist_7b <- ggplot(data, aes(x=ICC_Scen7_ELA1)) + 
-  geom_histogram(color="black", fill="white") +
-  geom_vline(aes(xintercept=0.45),
-             color="black", linetype="dashed", size=1) +
-  theme_bw() +
-  ylim(0,100000) +
-  labs(title = NULL, x = NULL, y = NULL) +
-  theme(axis.text.x=element_blank(), #remove x axis labels
-        axis.ticks.x=element_blank(), #remove x axis ticks
-        axis.text.y=element_blank(),  #remove y axis labels
-        axis.ticks.y=element_blank(), #remove y axis ticks
-        plot.title = element_text(hjust = 0.5))  
-
-hist_8a <- ggplot(data, aes(x=ICC_Scen8_ELA0)) + 
-  geom_histogram(color="black", fill="white") +
-  geom_vline(aes(xintercept=0.49),
-             color="black", linetype="dashed", size=1) +
-  theme_bw() +
-  ylim(0,100000) +
-  labs(title = "Scenario 8", x = NULL, y = NULL) +
-  theme(axis.text.x=element_blank(), #remove x axis labels
-        axis.ticks.x=element_blank(), #remove x axis ticks
-        axis.text.y=element_blank(),  #remove y axis labels
-        axis.ticks.y=element_blank(), #remove y axis ticks
-        plot.title = element_text(hjust = 0.5))  
-
-hist_8b <- ggplot(data, aes(x=ICC_Scen8_ELA1)) + 
-  geom_histogram(color="black", fill="white") +
-  geom_vline(aes(xintercept=0.44),
-             color="black", linetype="dashed", size=1) +
-  theme_bw() +
-  ylim(0,100000) +
-  labs(title = NULL, x = NULL, y = NULL) +
-  theme(axis.text.x=element_blank(), #remove x axis labels
-        axis.ticks.x=element_blank(), #remove x axis ticks
-        axis.text.y=element_blank(),  #remove y axis labels
-        axis.ticks.y=element_blank(), #remove y axis ticks
-        plot.title = element_text(hjust = 0.5))  
-
-hist_9a <- ggplot(data, aes(x=ICC_Scen9_ELA0)) + 
-  geom_histogram(color="black", fill="white") +
-  geom_vline(aes(xintercept=0.45),
-             color="black", linetype="dashed", size=1) +
-  theme_bw() +
-  ylim(0,100000) +
-  labs(title = "Scenario 9", x = NULL, y = NULL) +
-  theme(axis.text.x=element_blank(), #remove x axis labels
-        axis.ticks.x=element_blank(), #remove x axis ticks
-        axis.text.y=element_blank(),  #remove y axis labels
-        axis.ticks.y=element_blank(), #remove y axis ticks
-        plot.title = element_text(hjust = 0.5))  
-
-hist_9b <- ggplot(data, aes(x=ICC_Scen9_ELA1)) + 
-  geom_histogram(color="black", fill="white") +
-  geom_vline(aes(xintercept=0.43),
-             color="black", linetype="dashed", size=1) +
-  theme_bw() +
-  ylim(0,100000) +
-  labs(title = NULL, x = NULL, y = NULL) +
-  theme(axis.text.x=element_blank(), #remove x axis labels
-        axis.ticks.x=element_blank(), #remove x axis ticks
-        axis.text.y=element_blank(),  #remove y axis labels
-        axis.ticks.y=element_blank(), #remove y axis ticks
-        plot.title = element_text(hjust = 0.5))  
-
-### Combine All Histogram Plots ### ----
-ggarrange(hist_5a, hist_6a, hist_7a, hist_8a, hist_9a,
-          hist_5b, hist_6b, hist_7b, hist_8b, hist_9b,
-          ncol = 5, nrow = 2)
 
 ### Perform Statistical Testing ### ----
 ## Scenario 5 ##
